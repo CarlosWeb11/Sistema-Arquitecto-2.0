35d236205bae80497ecdc0894312bcd33759b11c
Carlos <carlosdanisb@gmail.com>
2026-04-13 21:24:42 -0600

    Primer commit

---
+++ ArquitectoSistema/src/main/java/com/arquitecto/ArquitectoSistemaApplication.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.arquitecto;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ *
+ * @author Carlos Daniel
+ */
+@SpringBootApplication
+public class ArquitectoSistemaApplication {
+     public static void main(String[] args) {
+
+        // Tema
+        FlatDarkLaf.setup();
+
+        // Spring con UI habilitada
+        ConfigurableApplicationContext context =
+            new SpringApplicationBuilder(ArquitectoSistemaApplication.class)
+                .headless(false)
+                .run(args);
+
+        // Mostrar ventana
+        java.awt.EventQueue.invokeLater(() -> {
+            context.getBean(com.arquitecto.vista.VentanaPrincipal.class)
+                   .setVisible(true);
+        });
+    }
+    
+}
